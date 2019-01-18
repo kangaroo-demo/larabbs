14b0a052d4aba18ccffd6405dc515afcc181e8f8
Kangaroo <kangaroo-demo@qq.com>
2019-01-18 07:31:41 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
+  @yield('styles')
+
 </head>
 
 <body>
@@ -33,6 +35,9 @@
 
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
+
+@yield('scripts')
+
 </body>
 
 </html>
