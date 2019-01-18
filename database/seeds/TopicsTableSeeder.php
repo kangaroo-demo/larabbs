e6da2577b9cc56143ce77ed8ccd87d1d8f7fe6d3
Kangaroo <kangaroo-demo@qq.com>
2019-01-18 02:52:44 +0000

    填充用户和话题数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -7,12 +7,30 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
-        });
 
+        // 所有用户 ID 数组
+        $user_ids = \App\Models\User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组
+        $category_ids = \App\Models\Category::all()->pluck('id')->toArray();
+
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index)
+            use ($user_ids, $category_ids, $faker) {
+
+                // 从用户 ID 数组中随机取出一个并赋值
+                $topic->user_id = $faker->randomElement($user_ids);
+
+                // 从分类 ID 数组中随机取出一个并赋值
+                $topic->category_id = $faker->randomElement($category_ids);
+            });
+
+        // 将数据集合转换为数组，并插入到数据库中
         Topic::insert($topics->toArray());
     }
 
