e0b2e8a7497ecc4729d2ec0180a1b7b3ebb48d8e
Kangaroo <kangaroo-demo@qq.com>
2019-03-18 09:21:26 +0000

    翻译队列化

--- app/Jobs/TranslateSlug.php
+++ app/Jobs/TranslateSlug.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs;
 
+use App\Handlers\SlugTranslateHandler;
+use App\Models\Topic;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -12,23 +14,30 @@ class TranslateSlug implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $topic;
+
     /**
-     * Create a new job instance.
+     * 初始化构造器
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Topic $topic)
     {
-        //
+        //队列任务构造器中接收了 Eloquent 模型，将只会序列化模型的ID
+        $this->topic = $topic;
     }
 
     /**
-     * Execute the job.
+     * 执行队列任务
      *
      * @return void
      */
     public function handle()
     {
-        //
+        //请求百度 API 接口进行翻译
+        $slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
+
+        //为了避免模型监控器死循环调用，我们使用 DB 类直接对数据库进行操作
+        \DB::table('topics')->where('id', $this->topic->id)->update(['slug' => $slug]);
     }
 }
