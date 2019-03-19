18655f2a90b92e416ed7b37463204a59202bd0f3
Kangaroo <kangaroo-demo@qq.com>
2019-03-19 09:53:01 +0000

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,11 +11,14 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        //净化数据，防止XSS
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
-    public function updating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+        //修改评论数
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 // 分类相关路由
 Route::resource('categories', 'CategoriesController', ['only' => 'show']);
 
-
-
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+// 回复相关路由
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
