c148272bae52c153a34d0df2bcf2c1b37fd0a451
Kangaroo <kangaroo-demo@qq.com>
2019-05-29 07:46:03 +0000

    API限制使用评率

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,15 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function ($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')->name('api.users.store');
+    //设置一分钟调用一次
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')->name('api.users.store');
+    });;
 });
