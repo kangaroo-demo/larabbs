631fa7c93e96b9a5c1e723145f8f27f1db7ae144
Kangaroo <kangaroo-demo@qq.com>
2019-05-29 10:12:04 +0000

    添加微信登录扩展

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class EventServiceProvider extends ServiceProvider
         ],
         Verified::class => [
             \App\Listeners\EmailVerified::class,
+        ],
+        SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
         ],
     ];
 
