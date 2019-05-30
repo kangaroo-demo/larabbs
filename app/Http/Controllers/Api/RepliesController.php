51019573a98e39d6aeb13cec123d64f45fdcf23b
Kangaroo <kangaroo-demo@qq.com>
2019-05-30 08:50:06 +0000

    replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -5,11 +5,49 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Models\Reply;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
 use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
+    /**
+     * 话题回复列表
+     * @param Topic $topic
+     * @return \Dingo\Api\Http\Response
+     */
+    public function index(Topic $topic, Request $request)
+    {
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $topic->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load($request->include);
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
+
+    /**
+     * 某个用户的回复列表
+     * @param User $user
+     * @return \Dingo\Api\Http\Response
+     */
+    public function userIndex(User $user, Request $request)
+    {
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $replies = $user->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load($request->include);
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
     /**
      * 发表回复
      * @param ReplyRequest $request
